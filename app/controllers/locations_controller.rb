3a0087275e7f6d4963e9f7807e9986e56003791a
otukutun <orehaorz@gmail.com>
2014-07-20 21:25:45 +0900

    add explanation how to fetch location data

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,6 +5,8 @@ class LocationsController < ApplicationController
 
   def fetch_location
     #@location = Location.find(:first)
+    #TODO flag=trueの最大件数を取得する
+    #max and flag=true の条件で1件取得
     @location = Location.first
     #@location = Location.where(flag: true).limit(1).order("RANDOM()")
     #@location = Location.where(:flag => true)
