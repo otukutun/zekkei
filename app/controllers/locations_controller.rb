f74d311f39d1ee50e5be712994bc64632f4366be
otukutun <orehaorz@gmail.com>
2014-07-20 21:48:43 +0900

    fetch location by random

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,12 +4,10 @@ class LocationsController < ApplicationController
   end
 
   def fetch_location
-    #@location = Location.find(:first)
     #TODO flag=trueの最大件数を取得する
     #max and flag=true の条件で1件取得
-    @location = Location.first
     #@location = Location.where(flag: true).limit(1).order("RANDOM()")
-    #@location = Location.where(:flag => true)
+    @location = Location.where(:flag => true).sample
   end
 
 end
