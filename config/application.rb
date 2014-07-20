b4cabb3e00518f29a816f50d18ee96d1d9c07c18
otukutun <orehaorz@gmail.com>
2014-07-20 13:26:26 +0900

    test

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Zekkei
   class Application < Rails::Application
-    config.serve_static_assets = true
+    #config.serve_static_assets = true
+    config.serve_static_assets = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
