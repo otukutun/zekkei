c679813cde1f17bc7f1f90a279291f4581d74ef7
otukutun <orehaorz@gmail.com>
2014-08-24 01:16:51 +0900

    fix size

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,4 @@
 class Location < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :large => "500x550#", :medium => "400x400#", :thumb => "100x100>" }
+  has_attached_file :photo, :styles => { :large => "1250x550#", :medium => "400x400#", :thumb => "100x100>" }
   validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
 end
